74f11a77d2154cc906133162a7567687e46f7593
Drew <drew@finstrom.us>
2015-05-27 16:17:34 -0700

    refactor to solve for non-returning recursive function

--- js/script.js
+++ js/script.js
@@ -12,3 +12,38 @@ $( document ).ready(function() {
 var foo = function(bar) {
   return false;
 };
+
+var primeSift = function(number) {
+  var numbers = [];
+  for (var i = 2; i <= number; i++) {
+    numbers.push(i);
+  }
+
+  var result = sift(numbers, 0);
+  console.log("result contains: " + result.toString());
+  return result;
+}
+
+var sift = function(primes, index) {
+  var prime = primes[index];
+
+  if (prime === primes[primes.length - 1]) {
+    console.log(primes.toString())
+    return primes;
+  } else {
+    primes.forEach(function(number) {
+      //console.log(number);
+      if (number !== prime && number % prime === 0) {
+        //console.log(number + ' is divisible by ' + prime + ' prime - pop it from array');
+        var i = primes.indexOf(number);
+        primes.splice(i, 1);
+      }
+    });
+
+    console.log("recursing with " + (index + 1).toString());
+    console.log("primes array contains: " + primes.toString());
+
+    return sift(primes, index + 1);
+  }
+
+}
--- spec/specs.js
+++ spec/specs.js
@@ -3,3 +3,12 @@ describe('foo', function() {
     expect(foo('bar')).to.equal(false);
   });
 });
+
+
+describe('primeSift', function() {
+  it("expects an array containing only prime numbers", function() {
+    expect(primeSift(4)).to.eql([2,3]);
+    expect(primeSift(9)).to.eql([2,3,5,7]);
+    expect(primeSift(22)).to.eql([2,3,5,7,11,13,17,19]);
+  });
+});
